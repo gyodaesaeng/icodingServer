de7bac90cbf0f83b2e62e53b70f94de321690a0f
jaejin0209 <jaejin0209@naver.com>
2020-08-21 14:57:38 +0900

    Update index.php

--- html/index.php
+++ html/index.php
@@ -4,6 +4,7 @@
  $snoopy = new Snoopy;
  $snoopy->fetch("https://www.acmicpc.net/user/jaejin0209");
  $txt=$snoopy->results;
+ var_dump($txt);
  $rex="/\<span class=\"problem_number\"\>(.*)\<\/span\>/";
  preg_match_all($rex,$txt,$o);
  var_dump($o);
