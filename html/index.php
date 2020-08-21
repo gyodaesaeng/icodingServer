a8596d4451cf79ffa2091ecdc2b76e3e1c6fffc1
gyodaesaeng <iadev20@gmail.com>
2020-08-21 22:13:59 +0900

    change index.php

--- html/index.php
+++ html/index.php
@@ -1,7 +1,14 @@
 <?php
+ error_reporting(E_ALL);
+ ini_set("display_errors", 1);
  include_once 'simple_html_dom.php';
+ $s = mysqli_connect("localhost", "root", "dlwowls1","DB");
  $html = file_get_html('https://www.acmicpc.net/user/jaejin0209');
- $ret = $html->find(div[class="panel-body"]);
- print($ret);
-}
+ $ret = $html->find('div[class=panel-body]');
+ foreach($ret[0]->find('span[class=problem_number]') as $element){
+	$n = $element->first_child()->innertext;
+	if((int)mysqli_query($s,"SELECT EXISTS (SELECT * FROM CORRECT WHERE ID='jaejin0209', PROBLEM={$n}) as success;")<1){
+		mysqli_query($s,"INSERT INTO CORRECT VALUES('jaejin0209',{$n});");
+	}
+ }
 ?>
