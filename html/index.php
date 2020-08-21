6398d3a939b4d10fa481d4387686350f3840010b
jaejin0209 <jaejin0209@naver.com>
2020-08-21 16:22:40 +0900

    add simple_html_dom

--- html/index.php
+++ html/index.php
@@ -1,11 +1,7 @@
 <?php
- include "Snoopy.class.php";
- $o="";
- $snoopy = new Snoopy;
- $snoopy->fetch("https://www.acmicpc.net/user/jaejin0209");
- $txt=$snoopy->results;
- var_dump($txt);
- $rex="/\<span class=\"problem_number\"\>(.*)\<\/span\>/";
- preg_match_all($rex,$txt,$o);
- var_dump($o);
+ include_once 'simple_html_dom.php';
+ $html = file_get_html('https://www.acmicpc.net/user/jaejin0209');
+ $ret = $html->find(div[class="panel-body"]);
+ print($ret);
+}
 ?>
