d86718b767f46b2eee754aac575e540985b3d14a
jaejin0209 <jaejin0209@naver.com>
2020-08-21 13:47:00 +0900

    Update index.php

--- html/index.php
+++ html/index.php
@@ -4,7 +4,7 @@
  $snoopy = new Snoopy;
  $snoopy->fetch("https://www.acmicpc.net/user/jaejin0209");
  $txt=$snoopy->results;
- $rex="/\<div class=\"panel-body\"\>*\<\/div\>/";
+ $rex="/\<div class=\"panel-body\"\>(.*)\<\/div\>/";
  preg_match_all($rex,$txt,$o);
  print_r($o[0][0]);
 ?>
