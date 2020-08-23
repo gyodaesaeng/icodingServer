f8844d9fce2a00b81c36f3b9c1960f49c16d451e
jaejin0209 <jaejin0209@naver.com>
2020-08-23 13:46:48 +0900

    update

--- html/index.php
+++ html/index.php
@@ -21,22 +21,23 @@
     <table>
       <thead>
         <tr>
-          <th>d</th>
-          <th>first_name</th>
-          <th>last_name</th>
-          <th>hire_date</th>
+          <th>아이디</th>
+          <th>이름</th>
+          <th>상태 메시지</th>
+          <th>맞은 문제</th>
+          <th>제출</th>
         </tr>
       </thead>
       <tbody>
 <?php
- error_reporting(E_ALL);
- ini_set("display_errors", 1);
- $s = mysqli_connect("localhost", "root", "dlwowls1","DB");
- $id = "jaejin0209";
-$view = mysqli_query($s,"SELECT * FROM USER_INFO ORDER BY CORRECT DESC");
-while($user = mysqli_fetch_array($view)){
-	echo "<tr><td>{$user['ID']}</td><td>{$user['NAME']}</td><td>{$user['STATUS_MESSAGE']}</td><td>{$user['CORRECT']}</td><td>{$user['SUBMIT']}</td></tr>";
-}
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+  $s = mysqli_connect("localhost", "root", "dlwowls1","DB");
+  $id = "jaejin0209";
+  $view = mysqli_query($s,"SELECT * FROM USER_INFO ORDER BY CORRECT DESC");
+  while($user = mysqli_fetch_array($view)){
+	   echo "<tr><td><a href='/user?id={$user['ID']}'>{$user['ID']}</a></td><td>{$user['NAME']}</td><td>{$user['STATUS_MESSAGE']}</td><td>{$user['CORRECT']}</td><td>{$user['SUBMIT']}</td></tr>";
+  }
 ?>
 </tbody>
     </table>
--- html/user.php
+++ html/user.php
@@ -4,42 +4,42 @@
 </form>
 
 <?php
- error_reporting(E_ALL);
- ini_set("display_errors", 1);
- include_once 'simple_html_dom.php';
- $s = mysqli_connect("localhost", "root", "dlwowls1","DB");
- $id = "jaejin0209";
- if(array_key_exists('update',$_POST)){
- $html = file_get_html("https://www.acmicpc.net/user/{$id}");
- $ret = $html->find('div[class=panel-body]');
- foreach($ret[0]->find('span[class=problem_number]') as $element){
-	$n = $element->first_child()->innertext;
-	if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM CORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))<1){
-		mysqli_query($s,"INSERT INTO CORRECT VALUES('{$id}',{$n});");
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+	include_once 'simple_html_dom.php';
+	$s = mysqli_connect("localhost", "root", "dlwowls1","DB");
+	$id = $_GET["id"];
+	if(array_key_exists('update',$_POST)){
+		$html = file_get_html("https://www.acmicpc.net/user/{$id}");
+		$ret = $html->find('div[class=panel-body]');
+		foreach($ret[0]->find('span[class=problem_number]') as $element){
+			$n = $element->first_child()->innertext;
+			if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM CORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))<1){
+				mysqli_query($s,"INSERT INTO CORRECT VALUES('{$id}',{$n});");
+			}
+			if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM INCORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))>0){
+				mysqli_query($s,"DELETE FROM INCORRECT WHERE ID='{$id}'AND PROBLEM={$n});");
+			}
+		}
+		foreach($ret[1]->find('span[class=problem_number]') as $element){
+			$n = $element->first_child()->innertext;
+			if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM INCORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))<1){
+				mysqli_query($s,"INSERT INTO INCORRECT VALUES('{$id}',{$n});");
+			}
+			if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM CORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))>0){
+				mysqli_query($s,"DELETE FROM CORRECT WHERE ID='{$id}'AND PROBLEM={$n});");
+			}
+		}
 	}
-	if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM INCORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))>0){
-		mysqli_query($s,"DELETE FROM INCORRECT WHERE ID='{$id}'AND PROBLEM={$n});");
+	$view = mysqli_query($s,"SELECT PROBLEM FROM CORRECT WHERE ID='{$id}' ORDER BY PROBLEM");
+	while($n = mysqli_fetch_array($view)){
+		echo $n[0];
+		echo " ";
 	}
- }
- foreach($ret[1]->find('span[class=problem_number]') as $element){
-	$n = $element->first_child()->innertext;
-	if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM INCORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))<1){
-		mysqli_query($s,"INSERT INTO INCORRECT VALUES('{$id}',{$n});");
+	echo "<br>";
+	$view = mysqli_query($s,"SELECT PROBLEM FROM INCORRECT WHERE ID='{$id}' ORDER BY PROBLEM");
+	while($n = mysqli_fetch_array($view)){
+		echo $n[0];
+		echo " ";
 	}
-	if(mysqli_num_rows(mysqli_query($s,"SELECT * FROM CORRECT WHERE ID='{$id}' AND PROBLEM={$n}"))>0){
-		mysqli_query($s,"DELETE FROM CORRECT WHERE ID='{$id}'AND PROBLEM={$n});");
-	}
- }
-}
-$view = mysqli_query($s,"SELECT PROBLEM FROM CORRECT WHERE ID='{$id}' ORDER BY PROBLEM");
-while($n = mysqli_fetch_array($view)){
-	echo $n[0];
-	echo " ";
-}
-echo "<br>";
-$view = mysqli_query($s,"SELECT PROBLEM FROM INCORRECT WHERE ID='{$id}' ORDER BY PROBLEM");
-while($n = mysqli_fetch_array($view)){
-	echo $n[0];
-	echo " ";
-}
 ?>
